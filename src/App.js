2009ecc6fb80aaac163850e0c196e47e600e31b7
saintlyzero <shubham.dalvi97@gmail.com>
2022-03-14 00:45:46 -0400

    add template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,87 @@
+import React from "react";
 import "./index.css";
 
-function App() {
-  return <h3>Let's Meet</h3>;
+const APP_NAME = "Let's Meet";
+
+function Heading() {
+  return <p className="heading">{APP_NAME}</p>;
 }
 
+function PeopleAdded() {
+  return (
+    <div className="container-people">
+      <PersonRecord />
+    </div>
+  );
+}
+function PersonRecord() {
+  return (
+    <div className="container-people">
+      <div className="row-person">
+        <label class="person-name">Shubham</label>
+        <label class="person-dates">10-02-2022 - 10-02-2023</label>
+        <button class="btn-delete-person">DL</button>
+      </div>
+    </div>
+  );
+}
+function AddPerson() {
+  return (
+    <table>
+      <tr>
+        <td>
+          <label>Name</label>
+        </td>
+        <td>
+          <input type="text" className="ip-name" placeholder="Enter Name" />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <label>Dates</label>
+        </td>
+        <td>
+          <input type="date" className="ip-date" />
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>
+          <button class="btn-add-item">Add</button>
+        </td>
+      </tr>
+    </table>
+  );
+}
+
+function RightSide() {
+  return (
+    <div className="right-side">
+      <AddPerson />
+      <PeopleAdded />
+    </div>
+  );
+}
+
+function LeftSide() {
+  return <div className="left-side"></div>;
+}
+
+function MainComponent() {
+  return (
+    <React.Fragment>
+      <Heading />
+      <div className="container-main">
+        <div className="container-1">
+          <LeftSide />
+          <RightSide />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function App() {
+  return <MainComponent />;
+}
 export default App;
---
+++ src/Models.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
+import { DateRangePicker } from "rsuite";
+
+class Person {
+  constructor(name, startDate, endDate) {
+    id = uuidv4();
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+}
